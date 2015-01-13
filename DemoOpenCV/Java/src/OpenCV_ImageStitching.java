356ac7b0618577dc95bae5976e01d0bec416f79a
nvkhoi <khoinvse61174@fpt.edu.vn>
2015-01-13 22:30:50 +0700

    - Thay doi cau truc thu muc - Them Demo C++ - Them thu muc tai lieu

---
+++ DemoOpenCV/Java/src/OpenCV_ImageStitching.java
@@ -0,0 +1,12 @@
+import org.opencv.core.*;
+
+public class OpenCV_ImageStitching {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Mat m  = Mat.eye(3, 3, CvType.CV_8UC1);
+        System.out.println("m = " + m.dump());
+	}
+}
+
